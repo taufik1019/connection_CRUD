65f447f12f29ab30aa959676de3a127f658234a5
taufik1019 <taufikandrirahman17@gmail.com>
2022-09-18 22:27:45 +0700

    innitial commit Create, Update, Delete

---
+++ login/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIBKMNet.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Update { get; set; }
+    }
+}
---
+++ login/Program.cs
@@ -0,0 +1,220 @@
+﻿using SIBKMNet.Models;
+using System;
+using System.Data.SqlClient;
+
+namespace SIBKMNet
+{
+    class Program
+    {
+        SqlConnection sqlConnection;
+
+        /*
+         * Data Source -> Server
+         * Initial Catalog -> Database
+         * User ID -> username
+         * Password -> password
+         * Connect Timeout
+         */
+        string connectionString = "Data Source=INBOOK_X2;Initial Catalog=SIBKMNET;User ID=sibkmnet;Password=1234567890;Connect Timeout=30";
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            //program.GetById(1);
+
+            Country country = new Country()
+            {
+                Name = "waktu indonesia bagian utara",
+                Update = "waktu indonesia bagian timur",
+                Id =7
+            };
+            //program.Insert(country);
+            program.Delete(country);
+
+            
+            //program.Pembatas();
+            
+            program.GetAll();
+
+            //program.Update(country);
+        }
+        public void Pembatas()
+        {
+            Console.WriteLine("---------------");
+        }
+
+
+        void GetAll()
+        {
+            string query = "SELECT * FROM Country";
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        void GetById(int id)
+        {
+            string query = "SELECT * FROM Country WHERE Id = @id";
+
+            SqlParameter sqlParameter = new SqlParameter();
+            sqlParameter.ParameterName = "@id";
+            sqlParameter.Value = id;
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.Add(sqlParameter);
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        void Insert(Country country)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@name";
+                sqlParameter.Value = country.Name;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "INSERT INTO Country " +
+                        "(Name) VALUES (@name)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        }
+
+      
+
+
+        // Delete
+        void Delete(Country country)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@id";
+                sqlParameter.Value = country.Id;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "DELETE Country " + "WHERE (Id) = (@id)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+            // update
+           void Update(Country country)
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                    SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                    sqlCommand.Transaction = sqlTransaction;
+
+                    SqlParameter sqlParameter = new SqlParameter();
+                    SqlParameter sqlParameter1 = new SqlParameter();
+                    SqlParameter sqlParameter2 = new SqlParameter();
+                    sqlParameter.ParameterName = "@name";
+                    sqlParameter.Value = country.Name;
+                    sqlParameter1.ParameterName = "@update";
+                    sqlParameter1.Value = country.Update;
+                    sqlParameter2.ParameterName = "@id";
+                    sqlParameter2.Value = country.Id;
+                    //Console.WriteLine(country.Update);
+                    //Console.WriteLine(country.Id);
+
+                    sqlCommand.Parameters.Add(sqlParameter);
+                    sqlCommand.Parameters.Add(sqlParameter1);
+                    sqlCommand.Parameters.Add(sqlParameter2);
+
+                    try
+                    {
+                        sqlCommand.CommandText = "UPDATE Country SET name = (@update) " + "WHERE Id = (@id)";
+                        sqlCommand.ExecuteNonQuery();
+                        sqlTransaction.Commit();
+
+                    }catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.InnerException);
+                    }
+                }
+            }
+        }
+    }
+}
